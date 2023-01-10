edaa132dc24b311882974a097e771bae6bbbecee
gauravb-77 <67025663+gauravb-77@users.noreply.github.com>
2023-01-10 11:28:03 +0530

    Add files via upload

---
+++ Guess The Number game using python.py
@@ -0,0 +1,25 @@
+# no of guesses = 9
+# print the number of guesses left
+# No of guesses he took to finish
+# after winning type as 'You Won'
+# after completing all attempts type as 'Game Over'
+
+comp_num = 24
+print('Welcome to The Number Guessing Game! In this game you have 9 guesses. So let\'s play the game.')
+i = 1
+while i <= 9:
+    num = int(input('Enter a number:\n'))
+    if i < 9:
+        if num < comp_num:
+            print('\nPlease increase your number\nYour no. of guesses left:', 9 - i)
+        elif num > comp_num:
+            print('\nPlease decrease your number\nYour no. of guesses left:', 9 - i)
+        else:
+            print('\nYou Won!\nYou took', i, 'guesses for this')
+            break
+    else:
+        if num == comp_num:
+            print('\nYou Won!\nYou took', i, 'guesses for this')
+        else:
+            print('\nGame Over!')
+    i = i + 1
